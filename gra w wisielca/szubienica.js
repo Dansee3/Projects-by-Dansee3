6a9166c79879e5789eace7c313eab8d9f4d7453c
Dansee3 <kam.kalbarczyk@gmail.com>
2023-05-02 19:27:12 +0200

    cleaned

--- gra w wisielca/szubienica.js
+++ gra w wisielca/szubienica.js
@@ -1,4 +1,3 @@
-
 let haslo = "Bez pracy nie ma kołaczy";
 haslo = haslo.toUpperCase();
 
@@ -11,14 +10,13 @@ let ile_skuch = 0;
 let yes = new Audio("yes.wav");
 let no = new Audio("no.wav");
 
-for (i=0; i<dlugosc; i++)
-{
-    if (haslo.charAt(i)==" ") haslo1 = haslo1 + " ";
-    else haslo1 = haslo1 + "-";
+for (i = 0; i < dlugosc; i++) {
+	if (haslo.charAt(i) == " ") haslo1 = haslo1 + " ";
+	else haslo1 = haslo1 + "-";
 }
 
-function wypisz_haslo(){
-    document.getElementById("plansza").innerHTML = haslo1;
+function wypisz_haslo() {
+	document.getElementById("plansza").innerHTML = haslo1;
 }
 
 window.onload = start;
@@ -61,70 +59,79 @@ litery[32] = "Z";
 litery[33] = "Ź";
 litery[34] = "Ż";
 
+function start() {
+	let tresc_diva = "";
 
-function start(){
+	for (i = 0; i <= 34; i++) {
+		let element = "lit" + i;
+		tresc_diva =
+			tresc_diva +
+			'<div class="litera" onclick="sprawdz(' +
+			i +
+			')"id="' +
+			element +
+			'">' +
+			litery[i] +
+			"</div>";
+		if ((i + 1) % 7 == 0)
+			tresc_diva = tresc_diva + '<div style="clear: both;"></div>';
+	}
 
-    let tresc_diva ="";
+	document.getElementById("alfabet").innerHTML = tresc_diva;
 
-    for (i=0; i<=34; i++){
-        let element = "lit" + i;
-        tresc_diva = tresc_diva + '<div class="litera" onclick="sprawdz('+i+')"id="'+element+'">'+litery[i]+'</div>';
-        if ((i+1) % 7==0) tresc_diva = tresc_diva + '<div style="clear: both;"></div>';
-    }
-
-    document.getElementById("alfabet").innerHTML = tresc_diva;
-
-    wypisz_haslo();
+	wypisz_haslo();
 }
 
-String.prototype.ustawZnak = function(miejsce, znak){
-    if(miejsce > this.length - 1) return this.toString();
-    else return this.substring(0, miejsce) + znak + this.substring(miejsce + 1);
+String.prototype.ustawZnak = function (miejsce, znak) {
+	if (miejsce > this.length - 1) return this.toString();
+	else return this.substring(0, miejsce) + znak + this.substring(miejsce + 1);
+};
+
+function sprawdz(nr) {
+	let trafiona = false;
+
+	for (i = 0; i < dlugosc; i++) {
+		if (haslo.charAt(i) == litery[nr]) {
+			haslo1 = haslo1.ustawZnak(i, litery[nr]);
+			trafiona = true;
+		}
+	}
+	if (trafiona == true) {
+		yes.play();
+		let element = "lit" + nr;
+		document.getElementById(element).style.background = "#003300";
+		document.getElementById(element).style.color = "#00C000";
+		document.getElementById(element).style.border = "3px solid #00C000";
+		document.getElementById(element).style.cursor = "default";
+
+		wypisz_haslo();
+	} else {
+		no.play();
+		let element = "lit" + nr;
+		document.getElementById(element).style.background = "#330000";
+		document.getElementById(element).style.color = "#C00000";
+		document.getElementById(element).style.border = "3px solid #C00000";
+		document.getElementById(element).style.cursor = "default";
+		document.getElementById(element).setAttribute("onclick", ";");
+
+		//skucha
+		ile_skuch++;
+		let obraz = "img/s" + ile_skuch + ".jpg";
+		document.getElementById("szubienica").innerHTML =
+			'<img src="' + obraz + '" alt="">';
+	}
+	//wygrana
+	if (haslo == haslo1)
+		document.getElegitmentById("alfabet").innerHTML =
+			"Brawo, odgadłeś hasło ! :" +
+			"<br /><br />" +
+			haslo +
+			'<span class="reset" onclick="location.reload()"> JESZCZE RAZ ? </span>';
+
+	//przegrana
+	if (ile_skuch >= 9)
+		document.getElementById("alfabet").innerHTML =
+			"Niestety nie wyszło..." +
+			"<br /><br />" +
+			'<span class="reset" onclick="location.reload()"> JESZCZE RAZ ? </span>';
 }
-
-function sprawdz(nr){
-
-    let trafiona = false;
-
-    for(i=0; i<dlugosc; i++)
-    {
-        if (haslo.charAt(i) == litery[nr]){
-            haslo1 = haslo1.ustawZnak(i,litery[nr]);
-            trafiona = true;
-        }
-    }
-    if(trafiona == true){
-        yes.play();
-        let element = "lit" + nr;
-        document.getElementById(element).style.background = "#003300";
-        document.getElementById(element).style.color = "#00C000";
-        document.getElementById(element).style.border = "3px solid #00C000";
-        document.getElementById(element).style.cursor = "default";
-
-        wypisz_haslo();
-    }
-    else{
-        no.play();
-        let element = "lit" + nr;
-        document.getElementById(element).style.background = "#330000";
-        document.getElementById(element).style.color = "#C00000";
-        document.getElementById(element).style.border = "3px solid #C00000";
-        document.getElementById(element).style.cursor = "default";
-        document.getElementById(element).setAttribute("onclick",";");
-
-        //skucha
-        ile_skuch++;
-        let obraz = "img/s" + ile_skuch + ".jpg";
-        document.getElementById("szubienica").innerHTML = '<img src="'+obraz+'" alt="">';
-    }
-    //wygrana
-    if(haslo == haslo1)
-    document.getElegitmentById("alfabet").innerHTML = "Brawo, odgadłeś hasło ! :"
-    +"<br /><br />"+haslo+'<span class="reset" onclick="location.reload()"> JESZCZE RAZ ? </span>';
-
-    //przegrana
-    if(ile_skuch>=9)
-    document.getElementById("alfabet").innerHTML = "Niestety nie wyszło..."+"<br /><br />"+'<span class="reset" onclick="location.reload()"> JESZCZE RAZ ? </span>';
-
-
-   }
--- weather app/code.js
+++ weather app/code.js
@@ -1,36 +1,33 @@
 let lat;
 let long;
-const apiKey = "6719d8b7087db025c6bd235f0a108459"
+const apiKey = "6719d8b7087db025c6bd235f0a108459";
 
-function startApp(){
-    
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                lat = position.coords.latitude;
-                long = position.coords.longitude;
+function startApp() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
 
-                console.log("lat: ", lat, "logn: ", long);
+            console.log("lat: ", lat, "logn: ", long);
 
-                getWeatherData()
-            }
-        );
-    };
-};
+            getWeatherData();
+        });
+    }
+}
 
-function getWeatherData(){
-let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}&lang=en`;
-console.log(url)
+function getWeatherData() {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}&lang=en`;
+    console.log(url);
 
- fetch(url).then( function(response){
-    response.json().then(function(data){
-        updateWeatherData(data)
-        console.log(data)
+    fetch(url).then(function (response) {
+        response.json().then(function (data) {
+            updateWeatherData(data);
+            console.log(data);
+        });
     });
- });
-};
+}
 
-function updateWeatherData(data){
+function updateWeatherData(data) {
     const city = data.name;
     const temp = data.main.temp;
     const airHumidity = data.main.humidity;
@@ -39,27 +36,28 @@ function updateWeatherData(data){
     const windSpeed = data.wind.speed;
     const sunrise = new Date(data.sys.sunrise * 1000);
     const sunset = new Date(data.sys.sunset * 1000);
-        
-    
-    
+
     const locationLink = document.getElementById("locationLink");
     locationLink.innerHTML = city;
     locationLink.href = `https://openstreetmap.org/#map=12/${lat}/${long}`;
 
-    document.getElementById("temp").innerHTML= temp + "&#186;" + "C";
+    document.getElementById("temp").innerHTML = temp + "&#186;" + "C";
 
-    document.getElementById("humidity").innerHTML= airHumidity + "%";
+    document.getElementById("humidity").innerHTML = airHumidity + "%";
 
-    document.getElementById("pressure").innerHTML= pressure + " hPa";
+    document.getElementById("pressure").innerHTML = pressure + " hPa";
 
-    document.getElementById("cloudsPrec").innerHTML= cloudiness + "%";
+    document.getElementById("cloudsPrec").innerHTML = cloudiness + "%";
 
-    document.getElementById("windSpeed").innerHTML= windSpeed + " km/h";
+    document.getElementById("windSpeed").innerHTML = windSpeed + " km/h";
 
-    document.getElementById("sunrise").innerHTML= sunrise.getHours() + ":" + sunrise.getMinutes() + " AM";
+    document.getElementById("sunrise").innerHTML =
+        sunrise.getHours() + ":" + sunrise.getMinutes() + " AM";
 
-    document.getElementById("sunset").innerHTML= sunset.getHours() + ":" + sunrise.getMinutes() + " AM";
+    document.getElementById("sunset").innerHTML =
+        sunset.getHours() + ":" + sunrise.getMinutes() + " AM";
 
-    let imgUrl = "https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png"
+    let imgUrl =
+        "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
     document.getElementById("currentWeatherImg").setAttribute("src", imgUrl);
-};
+}
